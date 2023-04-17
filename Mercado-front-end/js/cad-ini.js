b390d765724154713b3f7808e828576b0d83f7da
mateuSSRodrigues09 <126333038+mateuSSRodrigues09@users.noreply.github.com>
2023-04-17 14:21:55 -0300

    Update cad-ini.js

--- Mercado-front-end/js/cad-ini.js
+++ Mercado-front-end/js/cad-ini.js
@@ -41,15 +41,43 @@ inputrg.addEventListener('keypress', () => {
 
 })
 
+/* OLHAR SE ESTÁ VAZIO AS INPUTS GERENTE */
+const inpnome = document.querySelector('.inpnome')
+const inptel = document.querySelector('.inptel')
+const inpcpf = document.querySelector('.inpcpf')
+const inprg = document.querySelector('.inprg')
+const selgeren = document.querySelector('#funçao-funci')
 
-    const cadgerente = document.querySelector('.cadastro-gerente')
-    const cadfuncionario = document.querySelector('.cadastro-funcionario')
+/* ORGANIZAÇÃO BOX */
+
+
+
+btncontinuar.addEventListener('click', () => {
+    
+    if(inpnome || inptel || inpcpf || inprg || selgeren === ""){
+    alert('Por favor, digite todos os campos')
+}else{
+    boxgerente.classList.remove('active')
+    boxfuncionario.classList.add('active')
+}
+
+
+})
+
+function continuar(){
+    const inpnome = document.querySelector('.inpnome')
+    const inptel = document.querySelector('.inptel')
+    const inpcpf = document.querySelector('.inpcpf')
+    const inprg = document.querySelector('.inprg')
+    const selgeren = document.getElementById('funçao-funci')
+
+    const boxgerente = document.querySelector('.box-1.active')
+    const boxfuncionario = document.querySelector('.box-2')
     const btncontinuar = document.querySelector('.btn-prox')
 
-    btncontinuar.addEventListener('click', () => {
-        cadgerente.classList.add('active')
-    })
-
-    btncontinuar.addEventListener('click', () => {
-        cadfuncionario.classList.add('active')
-    })
+    if(inpnome.value || inptel.value || inpcpf.value || inprg.value == ""){
+        alert('Por favor, digite todos os campos')
+    }else if(selgeren.options[selgeren.selectedIndex].value == "" ){
+        alert('Selecione o campo para continuar.')
+    }
+}
